699a8a95b2abdc8a541dc1b86e5f9bf2a7ab1767
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2025-05-27 13:13:17 +0530

    Create yeet.meta.js

---
+++ yeet.meta.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        Flag Dialog Smokey Controls
+// @namespace   https://github.com/BillyDoesDev/
+// @description Adds Smokey status of a post and feedback options to flag dialogs.
+// @author      ArtOfCode
+// @contributor angussidney
+// @contributor rene
+// @contributor J F
+// @contributor Glorfindel
+// @attribution Brock Adams (https://github.com/BrockA)
+// @version     1.19.1
+// @updateURL   https://raw.githubusercontent.com/BillyDoesDev/greasyforks/main/yeet.meta.js
+// @downloadURL https://raw.githubusercontent.com/BillyDoesDev/greasyforks/main/yeet.js
+// @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
+// @match       *://*.stackexchange.com/*
+// @match       *://*.stackoverflow.com/*
+// @match       *://*.superuser.com/*
+// @match       *://*.serverfault.com/*
+// @match       *://*.askubuntu.com/*
+// @match       *://*.stackapps.com/*
+// @match       *://*.mathoverflow.net/*
+// @exclude     *://chat.stackexchange.com/*
+// @exclude     *://chat.meta.stackexchange.com/*
+// @exclude     *://chat.stackoverflow.com/*
+// @exclude     *://blog.stackoverflow.com/*
+// @exclude     *://*.area51.stackexchange.com/*
+// @exclude     *://stackexchange.com/*
+// @exclude     *://api.stackexchange.com/*
+// @exclude     *://data.stackexchange.com/*
+// @exclude     *://winterbash*.stackexchange.com/*
+// @require     https://cdn.rawgit.com/ofirdagan/cross-domain-local-storage/d779a81a6383475a1bf88595a98b10a8bd5bb4ae/dist/scripts/xdLocalStorage.min.js
+// @require     https://charcoal-se.org/userscripts/vendor/debug.min.js
+// @grant       none
+// ==/UserScript==
