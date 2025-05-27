8db25285192696a12810b9db48cfd63ec52a0cbe
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2025-05-27 13:20:06 +0530

    Update main.js

--- userscripts/csharp-training-form/main.js
+++ userscripts/csharp-training-form/main.js
@@ -5,6 +5,8 @@
 // @description  try to take over the world!
 // @author       BillyDoesDev
 // @match        https://docs.google.com/forms*
+// @updateURL    https://raw.githubusercontent.com/BillyDoesDev/greasyforks/main/userscripts/csharp-training-form/meta.js
+// @downloadURL  https://raw.githubusercontent.com/BillyDoesDev/greasyforks/main/userscripts/csharp-training-form/main.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
