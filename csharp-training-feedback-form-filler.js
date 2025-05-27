cc7ca200e06aa0a5acc4a5f620bb62fdd08a6e14
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2025-05-27 12:46:34 +0530

    Update csharp-training-feedback-form-filler.js

--- csharp-training-feedback-form-filler.js
+++ csharp-training-feedback-form-filler.js
@@ -5,6 +5,8 @@
 // @description  try to take over the world!
 // @author       BillyDoesDev
 // @match        https://docs.google.com/forms*
+// @updateURL    https://raw.githubusercontent.com/BillyDoesDev/greasyforks/refs/heads/main/csharp-training-feedback-form-filler.js
+// @downloadURL  https://raw.githubusercontent.com/BillyDoesDev/greasyforks/refs/heads/main/csharp-training-feedback-form-filler.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
