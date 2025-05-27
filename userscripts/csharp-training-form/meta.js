2a1b09b9fd3c4d4f5dee3906f3b2dbc37ecb659e
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2025-05-27 13:20:25 +0530

    Create meta.js

---
+++ userscripts/csharp-training-form/meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         C# Form Filler Thingy
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-26
+// @description  try to take over the world!
+// @author       BillyDoesDev
+// @match        https://docs.google.com/forms*
+// @updateURL    https://raw.githubusercontent.com/BillyDoesDev/greasyforks/main/userscripts/csharp-training-form/meta.js
+// @downloadURL  https://raw.githubusercontent.com/BillyDoesDev/greasyforks/main/userscripts/csharp-training-form/main.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// ==/UserScript==
