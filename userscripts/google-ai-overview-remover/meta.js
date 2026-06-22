442010835ee3ae35df04929d876f3982b2e4c6d7
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2026-06-22 13:41:00 +0530

    add meta.js

---
+++ userscripts/google-ai-overview-remover/meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Google AI Overview Remover
+// @namespace    http://tampermonkey.net/
+// @version      2026-06-22
+// @description  Remove the annoying google AI Overview
+// @author       BillyDoesDev
+// @match        https://www.google.com/search?*
+// @updateURL    https://raw.githubusercontent.com/BillyDoesDev/greasyforks/main/userscripts/google-ai-overview-remover/meta.js
+// @downloadURL  https://raw.githubusercontent.com/BillyDoesDev/greasyforks/main/userscripts/google-ai-overview-remover/_remover.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// ==/UserScript==
